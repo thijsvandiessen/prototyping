b2e8c3cf7b2837fe2cb213740ff72962282e40b7
Thijs van Diessen <thijs@Laras-MacBook-Pro.local>
2015-12-01 17:47:41 +0100

    first prototypes

---
+++ prototype 1/prototype 1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  prototype 1
+//
+//  Created by Thijs van Diessen on 01/12/15.
+//  Copyright © 2015 Thijs van Diessen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
